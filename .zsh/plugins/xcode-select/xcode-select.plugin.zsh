11bbfd68a9ac355fe2577077815672454eab38a7
Gabriele Canepa <git@gabrielecanepa.com>
2022-06-05 00:00:41 +0200

    zsh(plugin): updates to `google` and `xcode-select`

--- .zsh/plugins/xcode-select/xcode-select.plugin.zsh
+++ .zsh/plugins/xcode-select/xcode-select.plugin.zsh
@@ -6,14 +6,17 @@ xcode-select()
         echo "${fg[red]}Unknown option: $2$reset_color"
         return 1
       fi
+
       echo "${fg[yellow]}This will remove and reinstall the Xcode Command Line Tools$reset_color\n"
       printf "Are you sure you want to continue? (y/N) "
       read -r choice
+
       if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
-        sudo rm -rf $(xcode-select -print-path)
-        command xcode-select --install
+        sudo rm -rf "$(xcode-select -print-path)"
+        xcode-select --install
       fi
       ;;
+      
     *)
       command xcode-select $@
       ;;
