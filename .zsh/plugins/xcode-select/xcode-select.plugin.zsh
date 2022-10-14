667a17be84525f1874578ee11605ce302649d890
Gabriele Canepa <git@gabrielecanepa.com>
2022-10-14 18:05:30 +0200

    zsh(plugin): improve `xcode-select`

--- .zsh/plugins/xcode-select/xcode-select.plugin.zsh
+++ .zsh/plugins/xcode-select/xcode-select.plugin.zsh
@@ -1,22 +1,26 @@
-xcode-select() 
+xcode-select()
 {
   case $1 in
-    reset)
+    --reset)
       if [ "$2" ]; then
         echo "${fg[red]}Unknown option: $2$reset_color"
         return 1
       fi
 
-      echo "${fg[yellow]}This will remove and reinstall the Xcode Command Line Tools$reset_color\n"
+      echo "${fg[yellow]}warning$reset_color This will remove and reinstall the Xcode Command Line Tools"
       printf "Are you sure you want to continue? (y/N) "
       read -r choice
+      echo "\n"
 
-      if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
+      if [[ "${choice:l}" = "y" ]]; then
         sudo rm -rf "$(xcode-select -print-path)"
+        echo "xcode-select: note: command line tools removed"
         xcode-select --install
+      else
+        echo "xcode-select: note: operation aborted"
       fi
       ;;
-      
+
     *)
       command xcode-select $@
       ;;
