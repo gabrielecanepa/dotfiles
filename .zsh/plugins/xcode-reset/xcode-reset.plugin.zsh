cee2a813a7ba10023f3c6d9d2ebd3e74b01382d2
Gabriele Canepa <git@gabrielecanepa.com>
2023-04-09 16:47:16 +0200

    xcode-reset: rename plugin

--- .zsh/plugins/xcode-reset/xcode-reset.plugin.zsh
+++ .zsh/plugins/xcode-reset/xcode-reset.plugin.zsh
@@ -1,26 +1,20 @@
 #!/bin/sh
 
-function xcode-select() {
-  case $1 in
-    --reset)
-      if [ "$2" ]; then
-        echo "${fg[red]}Unknown option: $2$reset_color"
-        return 1
-      fi
+function xcode-reset() {
+  if [ "$2" ]; then
+    echo "${fg[red]}Unknown option: $2$reset_color"
+    return 1
+  fi
 
-      echo "${fg[yellow]}warning$reset_color This will remove and reinstall the Xcode Command Line Tools"
-      printf "Are you sure you want to continue? (y/N) "
-      read -r choice
+  echo "${fg[yellow]}warning$reset_color This will remove and reinstall the Xcode Command Line Tools."
+  printf "Are you sure you want to continue? (y/N) "
+  read -r choice
 
-      if [[ "${choice:l}" = "y" ]]; then
-        sudo rm -rf "$(xcode-select -print-path)"
-        echo "xcode-select: note: command line tools removed"
-        xcode-select --install
-      fi
-      ;;
+  if [[ "${choice:l}" = "y" ]]; then
+    sudo rm -rf "$(xcode-select -print-path)"
+    echo "xcode-select: note: command line tools removed"
+    xcode-select --install
+  fi
 
-    *)
-      command xcode-select $@
-      ;;
-  esac
+  unset choice
 }
