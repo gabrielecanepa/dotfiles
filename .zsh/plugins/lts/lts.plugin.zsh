b85eb0dec8e1362ffb2927b89de5153ed0364e7e
Gabriele Canepa <git@gabrielecanepa.com>
2023-10-13 19:02:14 +0200

    zsh(plugin): fixes and improvements to lts

--- .zsh/plugins/lts/lts.plugin.zsh
+++ .zsh/plugins/lts/lts.plugin.zsh
@@ -6,7 +6,7 @@ function lts() (
 
   local function validate_language() {
     if [[ -z "$1" ]] || ! [[ " ${LANGS[@]} " =~ " $1 " ]]; then
-      echo "${fg[red]}error${reset_color} You must specify a supported language."
+      echo "${fg[red]}error${reset_color} Invalid option: $1"
       return 1
     fi
   }
@@ -38,8 +38,34 @@ function lts() (
     esac
   }
 
+  local function get_language_name() {
+    case $1 in
+      node) echo "Node.js" ;;
+      ruby|python) echo "Ruby" ;;
+      *) return 1 ;;
+    esac
+  }
+
+  local function print_help_message() {
+    echo "lts is a command line utility to quickly find and install the latest stable version of a language.\n"
+
+    echo "Usage: lts [install] [language] [version]"
+    echo "Current language support: ${LANGS[@]}\n"
+
+    echo "Commands:"
+    echo "  lts <language> [version]            Get the specified or latest version of a language"
+    echo "  lts install                         Install the latest version of all languages"
+    echo "  lts install <languages>             Install the latest version of the specified languages"
+    echo "  lts install <language> [version]    Install the specified or latest version of a language"
+    echo "Examples:"
+    echo "  lts node                            Get the latest version of Node.js"
+    echo "  lts node 12                         Get latest version 12 of Node.js"
+    echo "  lts install node ruby               Install the latest version of Node.js and Ruby"
+    echo "  lts install ruby 2.7                Install latest 2.7 version of Ruby"
+  }
+
   case $1 in
-    install|upgrade)
+    install)
       [[ -z "$args" ]] && local langs=(${LANGS[@]}) || local langs=($args)
 
       # Check if all languages are supported.
@@ -52,48 +78,39 @@ function lts() (
         return 1
       fi
 
+      local updated=()
+
       for lang in $langs; do
-        [[ -z "$2" ]]
-        local prefix="$(echo $2 | cut -d. -f1,2)"
+        local vm=$(get_version_manager $lang)
+        local current=$($vm global)
+        local latest=$(get_latest_version $lang)
+        local lang_name=$(get_language_name $lang)
 
-        echo "${fg[blue]}info${reset_color} Installing latest ${(C)lang} version..."
+        echo "${fg[blue]}info${reset_color} Installing $lang_name v$latest..."
 
-        local current="$(lts $lang)"
+        $vm install $latest --skip-existing && $vm global $latest
 
-        case $lang in
-          node)
-            nodenv install $(lts node) --skip-existing && nodenv global $(lts node)
-            if [[ "$current" == "$(lts node)" ]]; then
-              echo "Already on latest version."
-            fi
-            ;;
-          ruby)
-            local current="$(lts ruby)"
-            rbenv install $(lts ruby) --skip-existing && rbenv global $(lts ruby)
-            if [[ "$current" == "$(lts ruby)" ]]; then
-              echo "Already on latest version."
-            fi
-            ;;
-          python)
-            local current="$(lts python)"
-            pyenv install $(lts python) --skip-existing && pyenv global $(lts python)
-            if [[ "$current" == "$(lts python)" ]]; then
-              echo "Already on latest version."
-            fi
-            ;;
-        esac
+        if [[ $current == $latest ]]; then
+          echo "Already on latest version."
+          continue
+        fi
 
-        echo ""
+        echo "${fg[green]} success${reset_color} Updated $lang_name from version $current to $latest."
       done
-
-      echo "${fg[green]}success${reset_color} Done! All languages are up-to-date."
       ;;
     *)
+      if [[ -z $1 ]] || [[ $1 =~ "^(-h|--help)$" ]]; then
+        print_help_message
+        return 0
+      fi
+
       local lang=$1
-      local vm=$(get_version_manager $lang)
 
       # Check the version manager.
       ! validate_language $lang && return 1
+
+      # Get the version manager.
+      local vm=$(get_version_manager $lang)
 
       # Extract the latest version.
       if [[ -z "$args" ]]; then
@@ -102,6 +119,10 @@ function lts() (
         local version="$($vm install --list | grep "^$args" | tail -1)"
       fi
 
-      [[ -z "$version" ]] && return 1 || echo $version
+
+      [[ -z "$version" ]] && return 1
+      
+      # Trim and print the latest version.
+      echo $version | sed -e "s/^[[:space:]]*//" -e "s/[[:space:]]*$//"
   esac
 )
