134a183121d172f4330306dd6b915e653a8bfcf9
Gabriele Canepa <git@gabrielecanepa.com>
2024-10-01 02:40:06 +0200

    zsh(plugin): refactor

--- .zsh/plugins/colors256/colors256.plugin.zsh
+++ .zsh/plugins/colors256/colors256.plugin.zsh
@@ -1,4 +1,4 @@
-#/bin/zsh
+#!/bin/zsh
 
 function colors256() {
   for code in {000..255}; do
--- .zsh/plugins/npm-global/npm-global.plugin.zsh
+++ .zsh/plugins/npm-global/npm-global.plugin.zsh
@@ -1,16 +1,22 @@
 #!/bin/zsh
 
-##
-# Plugin to easily manage global npm packages.
-#
-function npm() {
-  local NPM_GLOBAL="$HOME/.npm/global"
-  local INSTALL_CMDS=(add i in ins inst insta instal isnt isnta isntal isntall)
-  local UNINSTALL_CMDS=(uninstall unlink remove rm r un)
-  local UPDATE_CMDS=(update up upgrade udpate)
-  local GLOBAL_CMDS=(${INSTALL_CMDS[@]} ${UNINSTALL_CMDS[@]} ${UPDATE_CMDS[@]})
+function _npm() {
+  # Global directory
+  [[ -z "$NPM_GLOBAL" ]] && export NPM_GLOBAL="$HOME/.npm"
+  [[ ! -d "$NPM_GLOBAL" ]] && mkdir -p "$NPM_GLOBAL"
 
-  local npm_fresh() {
+  ##
+  # Local functions
+  #
+  local function _global_dependencies() {
+    local dependencies="$(cat "$NPM_GLOBAL/package.json" | jq -r '.dependencies | keys | .[]' | tr '\n' ' ')"
+    echo $dependencies
+  }
+
+  ##
+  # Command to update global npm packages.
+  #
+  function npm-fresh() {
     if [[ ! -z "${@:2}" ]]; then
       echo "Unknown arguments: ${@:2}"
       echo
@@ -18,121 +24,146 @@ function npm() {
       echo "  npm fresh"
       return 1
     fi
-    npm update --location=global
-    return $?
+    command npm update --location=global
   }
 
-  local pkg_dependencies() {
-    cat ./package.json | jq -r '.dependencies | keys | .[]' | tr '\n' ' '
+  ##
+  # Command to dump global npm packages.
+  #
+  function npm-dump() {
+    ! command -v jq >/dev/null && echo "Error: jq is required to dump global npm packages." && return 1
+
+    local dir="$(pwd)"
+    local version="$(cd ~ && echo ${$(node -v)#v})"
+    local dependencies=($(_global_dependencies))
+
+    case $1 in
+      install|uninstall|update)
+        local cmd=$1
+        local args=(${@:2})
+        ;;
+      *)
+        local args=($@)
+        ;;
+    esac
+
+    # Check for --node-version
+    for arg in $args; do
+      if [[ $arg == --node-version=* ]]; then
+        version=${arg#--node-version=}
+        break
+      fi
+    done
+
+    if [[ -z "$cmd" && ! -z "${args[@]}" ]]; then
+      echo "Unknown arguments: ${args[@]}"
+      echo
+      echo "To dump all global dependencies, run:"
+      echo "  npm dump <cmd> [options]"
+      return 1
+    fi
+
+    local package_content="{\"dependencies\":{"
+
+    for dependency in $dependencies; do
+      package_content+="\"$dependency\": \"*\""
+      [[ $dependency != $dependencies[-1] ]] && package_content+=","
+    done
+    
+    package_content+="}}"
+
+    echo $package_content | jq . > $NPM_GLOBAL/package.json
+
+    # Set up the version directory.
+    # local version_path="$NPM_GLOBAL/global/$version"
+    # [[ ! -d "$version_path" ]] && mkdir -p "$version_path"
+    # cd "$version_path"
+    # echo "{}" > "$version_path/package.json"
+
+    # command npm pkg set engines.node=$version
+    # command npm install $dependencies --package-lock-only >/dev/null
+    # exit=$?
+
+    # cd "$dir"
+    # return $exit
   }
 
-  local set_pkg_engine() {
-    local engine="$(command npm pkg get engines.node | sed -e 's/[{}"\n]//g')"
-    [[ $? != 0 ]] && command npm pkg get engines.node && echo && return $?
-    [[ -z "$engine" ]] && command npm pkg set engines.node=$version
+  ##
+  # npm command override to track and manage global packages.
+  #
+  function npm() {
+    local args=($@)
+    local cmd=
+    local params=()
+    local opts=()
+    local global=false
+
+    for arg in $args; do
+      if [[ $arg == --global ]]; then
+        global=true
+        continue
+      fi
+      if [[ $arg =~ ^-[\w\d]*g[\w\d]*$ ]]; then
+        global=true
+        [[ $arg != -g ]] && opts+=("${arg//-g/}")
+        continue
+      fi
+      if [[ $arg =~ ^--?[\w\d]+$ ]]; then
+        opts+=($arg)
+        continue
+      fi
+      params+=($arg)
+    done
+
+    local cmd=${params[1]}
+    local pkgs=${params[@]:1}
+
+    if [[ $global != true && ! $cmd =~ ^(dump|fresh)$ ]]; then
+      command npm $@
+      return $?
+    fi
+
+    case $cmd in
+      dump|fresh)
+        eval "npm-$cmd $@"
+        ;;
+      install|add|i|in|ins|inst|insta|instal|isnt|isnta|isntal|isntall|uninstall|unlink|remove|rm|r|un|update|up|upgrade|udpate)
+        if [[ ${#pkgs} == 0 ]]; then
+          command npm -g $cmd $(_global_dependencies)
+        else
+          command npm $@
+        fi
+        exit=$?
+        npm-dump >/dev/null
+        return $exit
+        ;;
+      *)
+        command npm $@
+        ;;
+    esac
   }
-
-  if [[ $1 == fresh ]]; then
-    npm_fresh $@
-    return $?
-  fi
-
-  local dir="$(pwd)"
-  local args=($@)
-
-  # Initialize params and options.
-  local params=()
-  local opts=()
-  local global=false
-
-  # Parse arguments.
-  for arg in $args; do
-    if [[ $arg == --global ]]; then
-      local global=true
-      continue
-    fi
-    if [[ $arg =~ ^-[\w\d]*g[\w\d]*$ ]]; then
-      local global=true
-      [[ $arg != -g ]] && opts+=("${arg//-g/}")
-      continue
-    fi
-    if [[ $arg =~ ^--?[\w\d]+$ ]]; then
-      opts+=($arg)
-      continue
-    fi
-    params+=($arg)
-  done; unset arg
-
-  local cmd="$params[1]"
-  local pkgs=($params[2,-1])
-
-  # Run original command and exit if not global.
-  if [[ $global != true || ! ${GLOBAL_CMDS[@]} =~ $cmd ]]; then
-    command npm $@
-    return $?
-  fi
-
-  # Get global version and installation directory.
-  cd "$HOME"
-  local version="${$(node -v)#v}"
-  local version_path="$NPM_GLOBAL/$version"
-  [[ ! -d "$version_path" ]] && mkdir -p "$version_path"
-  cd "$version_path"
-
-  # Check if is an installation cmd
-  if [[ $cmd =~ ${INSTALL_CMDS[@]} && ${#pkgs[@]} == 0 ]]; then
-    if [[ -f package.json ]]; then
-      npm --global install $(pkg_dependencies)
-    else
-      touch package.json
-    fi
-  else
-    [[ ! $opts =~ --package-lock-only ]] && opts+=("--package-lock-only")
-    command npm $params $opts &>/dev/null
-  fi
-
-  local exit=$?
-  set_pkg_engine
-  cd "$dir"
-  return $exit
-
-
-
-
-
-
-
-
-
-
-
-
-  # Run original command and exit if not global.
-  command npm $@
-  local npm_exit=$?
-
-  if [[ $npm_exit != 0 || $global != true || ! ${GLOBAL_CMDS[@]} =~ $cmd ]]; then
-    return $npm_exit
-  fi
-
-  # Get global node version and npm installation directory.
-  cd "$HOME"
-  local version="${$(node -v)#v}"
-  local version_path="$NPM_GLOBAL/$version"
-  [[ ! -d "$version_path" ]] && mkdir -p "$version_path"
-
-  # Switch to directory and dump packages.
-  cd "$version_path"
-
-
-  [[ ! $opts =~ --package-lock-only ]] && opts+=("--package-lock-only")
-  command npm $params $opts &>/dev/null
-
-  # Set node engine.
-  local engine="$(command npm pkg get engines.node | sed -e 's/[{}"\n]//g')"
-  [[ $? != 0 ]] && command npm pkg get engines.node && echo && return $?
-  [[ -z "$engine" ]] && command npm pkg set engines.node=$version
-
-  cd "$dir"
-  return $npm_exit
 }
+
+function _nodenv() {
+  ##
+  # Function override to align global npm packages among different node versions.
+  #
+  function nodenv() {
+    local CMDS=(global install)
+    local cmd=$1
+    local version=$2
+    local versions=(${@:2})
+    local current=$(command nodenv global)
+
+    command nodenv $@
+    exit=$?
+    [[ $exit != 0 ]] && return $exit
+    [[ $(command nodenv global) == $current || ! $cmd =~ ${^CMDS} ]] && return 0
+
+    npm -g install >/dev/null
+  }
+}
+
+command -v npm >/dev/null && _npm
+command -v nodenv >/dev/null && _nodenv
+unset -f _npm _nodenv
